405ed566fec7e15ee185751c24b5cc0d56d422e9
Matveev Andrey <circlsun@gmail.com>
2023-03-06 22:27:55 +0300

    Dry code

--- create_intent.py
+++ create_intent.py
@@ -13,7 +13,6 @@ def create_intent(project_id, display_name, training_phrases_parts, message_text
     training_phrases = []
     for training_phrases_part in training_phrases_parts:
         part = dialogflow.Intent.TrainingPhrase.Part(text=training_phrases_part)   # noqa
-        # Here we create a new training phrase for each provided part.
         training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part])
         training_phrases.append(training_phrase)
 
@@ -27,7 +26,10 @@ def create_intent(project_id, display_name, training_phrases_parts, message_text
     )
 
     response = intents_client.create_intent(
-        request={"parent": parent, "intent": intent}
+        request={
+            "parent": parent,
+            "intent": intent
+        }
     )
 
     print(f"Intent created: {response}")
@@ -37,14 +39,12 @@ def main():
     load_dotenv()
     project_id = os.getenv('PROJECT_ID')
 
-    with open("phrases.json", "r") as file:
+    with open("phrases.json", "r", encoding='UTF-8') as file:
         training_phrases = json.load(file)
 
     for display_name in training_phrases.keys():
         training_phrases_parts = training_phrases[display_name]["questions"]
         message_texts = training_phrases[display_name]["answer"]
-        print(training_phrases_parts)
-        print(message_texts)
 
         create_intent(
             project_id,
